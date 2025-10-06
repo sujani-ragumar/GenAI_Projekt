69fbc32f4a969e448b9f3f4a5a832d4c418007d1
prhin <prhiner@gmail.com>
2025-10-06 16:33:08 +0200

    Add code + .gitignore; ignore VS workspace

---
+++ RAG_test_philippe.py
@@ -0,0 +1,15 @@
+﻿from langchain_community.tools import DuckDuckGoSearchResults
+from langchain.agents import tool
+from openai import OpenAI
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("CEREBRAS_API_KEY")
+
+if api_key:
+    print("✅ API-Key wurde gefunden!")
+    print("Erste 5 Zeichen:", api_key[:5], "...")
+else:
+    print("❌ Kein API-Key gefunden.")
